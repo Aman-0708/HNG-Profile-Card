0cd0c8ffb4c7db6070b59e23a12dc222c678111b
Aman Oladele <amanoladele2021@gmail.com>
2025-10-18 11:42:26 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const timeDiv = document.getElementById('time');
+
+function updateTime() {
+    const currentTime = Date.now();
+    timeDiv.textContent = "Current Time is " + currentTime;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
